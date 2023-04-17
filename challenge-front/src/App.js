13a2fb5c54d6238a447ca2634285ab77eed09465
YacineSoussi <Yacine1.soussi@gmail.com>
2023-04-17 19:38:52 +0200

    ten if

--- challenge-front/src/App.js
+++ challenge-front/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Marketplace
+          Marketplace.com
         </a>
       </header>
     </div>
