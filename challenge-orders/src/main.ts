9ec4fb2c6bece9ef6d3d8857f78733e1f84e246d
alilou-dev <raidali95@yahoo.com>
2023-06-17 12:13:17 +0200

    somes fixes and init microservice orders

--- challenge-orders/src/main.ts
+++ challenge-orders/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    name: 'ORDERS_SERVICE',
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rmq-service:5672'],
+      queue: 'orders_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
