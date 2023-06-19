8314de1376398be4a5cd1ae31dc634ac4e383c7d
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-20 00:00:59 +0200

    Implement verification of login form

--- challenge-front/src/pages/LoginPage.js
+++ challenge-front/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import "../styles/auth/login.css";
+import "../styles/auth/auth.css";
 import "../styles/auth/util.css";
 
 import grocery from "../images/auth/grocery.png";
@@ -6,7 +6,42 @@ import { useState } from "react";
 
 function LoginPage() {
 	const [isVisible, setIsVisible] = useState(false);
+	const [emailError, setEmailError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
 	const handleClick = () => setIsVisible(!isVisible);
+
+	/**
+	 * Check if the email is valid
+	 * @param { string } value The email to check
+	 */
+	const isValidEmail = (value) => {
+		if (value === "") {
+			setEmailError("");
+		} else {
+			const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+			if (regex.test(value)) {
+				setEmailError("");
+			} else {
+				setEmailError("Entrer une adresse mail valide");
+			}
+		}
+	};
+
+	/**
+	 * Check if the password is valid
+	 * @param { string } value The password to check
+	 */
+	const isValidPassword = (value) => {
+		if (value === "") {
+			setPasswordError("");
+		} else {
+			if (value.length >= 6) {
+				setPasswordError("");
+			} else {
+				setPasswordError("Le mot de passe doit contenir au moins 6 caractères");
+			}
+		}
+	};
 
 	return (
 		<>
@@ -24,9 +59,18 @@ function LoginPage() {
 									type="text"
 									name="email"
 									placeholder="Adresse mail"
+									onInput={(e) => isValidEmail(e.target.value)}
 								/>
 								<span className="focus-input100"></span>
 							</div>
+							{emailError !== "" && (
+								<div
+									className="text-danger"
+									style={{ marginTop: "-20px", marginBottom: "20px" }}
+								>
+									{emailError}
+								</div>
+							)}
 							<div className="wrap-input100 validate-input">
 								<span className="btn-show-pass" onClick={handleClick}>
 									{isVisible ? (
@@ -40,9 +84,18 @@ function LoginPage() {
 									type={isVisible ? "text" : "password"}
 									name="pass"
 									placeholder="Mot de passe"
+									onInput={(e) => isValidPassword(e.target.value)}
 								/>
 								<span className="focus-input100"></span>
 							</div>
+							{passwordError !== "" && (
+								<div
+									className="text-danger"
+									style={{ marginTop: "-20px", marginBottom: "20px" }}
+								>
+									{passwordError}
+								</div>
+							)}
 							<div className="container-login100-form-btn">
 								<div className="wrap-login100-form-btn">
 									<div className="login100-form-bgbtn"></div>
