f15520f62ada7bcc3041ceed46802d17bb8495a8
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-19 11:31:34 +0200

    Refactor project and implement header menu

--- challenge-front/src/App.js
+++ challenge-front/src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import $ from "jquery";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+$(function () {
+	const header = $(".header");
+	let menuActive = false;
+
+	setHeader();
+	initMenu();
+
+	/**
+	 * Events
+	 */
+	$(window).on("resize", () => setHeader());
+	$(document).on("scroll", () => setHeader());
+
+	/**
+	 * Set header class
+	 */
+	function setHeader() {
+		if ($(window).scrollTop() > 100) {
+			header.addClass("scrolled");
+		} else {
+			header.removeClass("scrolled");
+		}
+	}
+
+	/**
+	 * Init burger menu button
+	 */
+	function initMenu() {
+		if ($(".hamburger").length) {
+			handleHamburgerClick();
+			handlePageMenu();
+		}
+	}
+
+	/**
+	 * Handle hamburger click
+	 */
+	function handleHamburgerClick() {
+		const hamburger = $(".hamburger");
+
+		hamburger.on("click", function (event) {
+			event.stopPropagation();
+
+			if (!menuActive) {
+				openMenu();
+
+				$(document).one("click", function cls(e) {
+					if ($(e.target).hasClass("menu_mm")) {
+						$(document).one("click", cls);
+					} else {
+						closeMenu();
+					}
+				});
+			} else {
+				$(".menu").removeClass("active");
+				menuActive = false;
+			}
+		});
+	}
+
+	/**
+	 * Handle page menu
+	 */
+	function handlePageMenu() {
+		if ($(".page_menu_item").length) {
+			const items = $(".page_menu_item");
+			items.each(function () {
+				const item = $(this);
+
+				item.on("click", function (evt) {
+					if (item.hasClass("has-children")) {
+						evt.preventDefault();
+						evt.stopPropagation();
+						const subItem = item.find("> ul");
+						if (subItem.hasClass("active")) {
+							subItem.toggleClass("active");
+						} else {
+							subItem.toggleClass("active");
+						}
+					} else {
+						evt.stopPropagation();
+					}
+				});
+			});
+		}
+	}
+
+	/**
+	 * Open burger menu
+	 */
+	function openMenu() {
+		const fs = $(".menu");
+		fs.addClass("active");
+		menuActive = true;
+	}
+
+	/**
+	 * Close burger menu
+	 */
+	function closeMenu() {
+		const fs = $(".menu");
+		fs.removeClass("active");
+		menuActive = false;
+	}
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<Menu />
+		</>
+	);
 }
 
 export default App;
