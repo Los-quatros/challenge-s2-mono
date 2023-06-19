0acceb5a8db87414a1be852ba97267cf43e6a8fd
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-19 17:30:15 +0200

    Remove package bootstrap and use instead the min.css file

--- challenge-front/src/index.js
+++ challenge-front/src/index.js
@@ -1,6 +1,4 @@
 import "font-awesome/css/font-awesome.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 
 import App from "./App";
 import React from "react";
