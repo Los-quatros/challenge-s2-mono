8b90c204e16af93816cb0bce8abf6e0ff9b75273
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-19 09:16:55 +0200

    Refactor initialization files of project

--- challenge-front/src/App.js
+++ challenge-front/src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Marketplace</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Marketplace.fr 
+          Learn React
         </a>
       </header>
     </div>
