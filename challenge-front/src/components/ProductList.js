2eb7f0964f4afaabf6728f1158ee698ef606d4a4
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-21 17:10:28 +0200

    Remove isotope

--- challenge-front/src/components/ProductList.js
+++ challenge-front/src/components/ProductList.js
@@ -2,28 +2,6 @@ import { useEffect, useState } from "react";
 
 import Product from "./Product";
 
-const $ = window.$;
-
-/**
- * Init isotope
- */
-function initIsotope() {
-	if ($(".product_grid").length) {
-		$(".product_grid").isotope({
-			itemSelector: ".product",
-			layoutMode: "fitRows",
-			fitRows: {
-				gutter: 30,
-			},
-			animationOptions: {
-				duration: 750,
-				easing: "linear",
-				queue: false,
-			},
-		});
-	}
-}
-
 function ProductList(props) {
 	const [products, setProducts] = useState([]);
 
@@ -31,20 +9,12 @@ function ProductList(props) {
 		setProducts(props.products);
 	}, [props.products]);
 
-	useEffect(() => {
-		if (products.length > 0) {
-			setTimeout(() => {
-				initIsotope();
-			}, 500);
-		}
-	}, [products]);
-
 	return (
 		<div className="products">
 			<div className="container">
 				<div className="row">
 					<div className="col">
-						<div className="product_grid">
+						<div className="product_grid d-flex justify-content-between flex-wrap">
 							{products.map((product, index) => (
 								<Product key={index} product={product} />
 							))}
