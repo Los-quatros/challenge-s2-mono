fe340c98394e7d70b8e23223ee2dff6dbf07f287
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-23 10:29:20 +0200

    Add missed file in last commit

--- challenge-front/src/App.js
+++ challenge-front/src/App.js
@@ -9,21 +9,18 @@ import { Suspense, lazy, useEffect } from "react";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Menu from "./components/Menu";
-import { gsap } from "gsap";
 
 const Login = lazy(() => import("./components/Login"));
 const Register = lazy(() => import("./components/Register"));
 const Home = lazy(() => import("./pages/HomePage.js"));
 const Categories = lazy(() => import("./pages/Categories"));
+
 const $ = window.$;
 
 window.$(function () {
 	const header = $(".header");
-	let menuActive = false;
 
 	setHeader();
-	initMenu();
 
 	/**
 	 * Events
@@ -40,90 +37,6 @@ window.$(function () {
 		} else {
 			header.removeClass("scrolled");
 		}
-	}
-
-	/**
-	 * Init burger menu button
-	 */
-	function initMenu() {
-		if ($(".hamburger").length) {
-			handleHamburgerClick();
-			handlePageMenu();
-		}
-	}
-
-	/**
-	 * Handle hamburger click
-	 */
-	function handleHamburgerClick() {
-		const hamburger = $(".hamburger");
-
-		hamburger.on("click", function (event) {
-			event.stopPropagation();
-
-			if (!menuActive) {
-				openMenu();
-
-				$(document).one("click", function cls(e) {
-					if ($(e.target).hasClass("menu_mm")) {
-						$(document).one("click", cls);
-					} else {
-						closeMenu();
-					}
-				});
-			} else {
-				$(".menu").removeClass("active");
-				menuActive = false;
-			}
-		});
-	}
-
-	/**
-	 * Handle page menu
-	 */
-	function handlePageMenu() {
-		if ($(".page_menu_item").length) {
-			const items = $(".page_menu_item");
-			items.each(function () {
-				const item = $(this);
-
-				item.on("click", function (evt) {
-					if (item.hasClass("has-children")) {
-						evt.preventDefault();
-						evt.stopPropagation();
-						const subItem = item.find("> ul");
-						if (subItem.hasClass("active")) {
-							subItem.toggleClass("active");
-							gsap.to(subItem, { duration: 0, height: 0 });
-						} else {
-							subItem.toggleClass("active");
-							gsap.to(subItem, { height: "auto" });
-							gsap.to(subItem, { duration: 0, height: 0 });
-						}
-					} else {
-						evt.stopPropagation();
-					}
-				});
-			});
-		}
-	}
-
-	/**
-	 * Open burger menu
-	 */
-	function openMenu() {
-		const fs = $(".menu");
-		fs.addClass("active");
-		menuActive = true;
-	}
-
-	/**
-	 * Close burger menu
-	 */
-	function closeMenu() {
-		const fs = $(".menu");
-		fs.removeClass("active");
-		menuActive = false;
 	}
 });
 
@@ -187,7 +100,6 @@ const AppContent = () => {
 	return (
 		<Suspense fallback={<span>...</span>}>
 			{displayHeader && <Header />}
-			{displayHeader && <Menu />}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				{isAuth && <Route path="/login" element={<Login />} />}
