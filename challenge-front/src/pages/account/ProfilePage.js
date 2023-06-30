07ea93568ffbef16cef8bf17bab1f4ade39f6fc6
Pereira Júlio <julioocesarr@outlook.fr>
2023-06-30 11:31:36 +0200

    Initialize user profile view

--- challenge-front/src/pages/account/ProfilePage.js
+++ challenge-front/src/pages/account/ProfilePage.js
@@ -14,25 +14,91 @@ function ProfilePage({ accountMenuChange, menu }) {
 	return (
 		<div id="content" className="p-4 p-md-5">
 			<SidebarPage sidebarMenuChange={handleProfileMenuChange} menu={menu} />
-			<h2 className="mb-4">Sidebar #01</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
+			<div>
+				<div className="row">
+					<div className="col-12">
+						<form className="file-upload">
+							<div className="row gx-5">
+								<div className="col-12 mb-3 mb-xxl-0">
+									<div className="bg-secondary-soft rounded">
+										<div className="row g-3">
+											<div className="col-12">
+												<label htmlFor="lastName" className="form-label">
+													Nom<span className="red">*</span>
+												</label>
+												<input
+													type="text"
+													className="form-control"
+													placeholder="John"
+													id="lastName"
+												/>
+											</div>
+										</div>
+										<div className="row g-3 mt-2">
+											<div className="col-12">
+												<label htmlFor="firstName" className="form-label">
+													Prénom<span className="red">*</span>
+												</label>
+												<input
+													type="text"
+													className="form-control"
+													placeholder="Doe"
+													id="firstName"
+												/>
+											</div>
+										</div>
+										<div className="row g-3 mt-2">
+											<div className="col-12">
+												<label htmlFor="email" className="form-label">
+													Adresse email<span className="red">*</span>
+												</label>
+												<input
+													type="email"
+													className="form-control"
+													id="email"
+													placeholder="johndoe@hotmail.fr"
+												/>
+											</div>
+										</div>
+										<div className="row g-3 mt-2">
+											<div className="col-12">
+												<label htmlFor="password" className="form-label">
+													Nouveau mot de passe<span className="red">*</span>
+												</label>
+												<input
+													placeholder="******"
+													type="password"
+													className="form-control"
+													id="password"
+												/>
+											</div>
+										</div>
+										<div className="row g-3 mt-2">
+											<div className="col-12">
+												<label htmlFor="confirmPassword" className="form-label">
+													Confirmation mot de passe{" "}
+													<span className="red">*</span>
+												</label>
+												<input
+													placeholder="******"
+													type="password"
+													className="form-control"
+													id="confirmPassword"
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="gap-3 d-md-flex text-center">
+								<button className="button profile_button">
+									<span>Mettre à jour</span>
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
