b24770fba6e408fe41434e656e41d617c38609d5
moncef-24s <m.saiki@24s.com>
2023-07-02 18:26:19 +0200

    add multer

--- challenge-images/src/images/images.module.ts
+++ challenge-images/src/images/images.module.ts
@@ -3,23 +3,26 @@ import { Module } from '@nestjs/common';
 import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
 
   imports: [ClientsModule.register([
     {
-        name: 'IMAGES_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-            urls: [`amqp://rmq-service:5672`],
-            queue: 'images_queue',
-            queueOptions: {
-              durable: false
-            }
-        }
-    }
-  ]),],
+      name: 'IMAGES_SERVICE',
+      transport: Transport.RMQ,
+      options: {
+          urls: [`amqp://rmq-service:5672`],
+          queue: 'images_queue',
+          queueOptions: {
+            durable: false
+          }
+      }
+    }]),
+    MulterModule.register({
+      dest: './uploads/',
+    }),
+  ],
   providers: [ImagesService],
   controllers: [ImagesController],
 })
