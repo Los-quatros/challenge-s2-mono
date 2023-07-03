d8db8705fdfa41c7f22dba86259af0e8328952bf
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-03 10:56:50 +0200

    Initialize returns view

--- challenge-front/src/pages/account/ReturnsPage.js
+++ challenge-front/src/pages/account/ReturnsPage.js
@@ -1,6 +1,12 @@
-import SidebarPage from "./HeaderPage";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect, useState } from "react";
+
+import HeaderPage from "./HeaderPage";
+import headphone1 from "../../assets/images/categories/headphones/headphone_6.png"; // TODO remove
+import headphone2 from "../../assets/images/categories/headphones/headphone_8.png"; // TODO remove
+import phone1 from "../../assets/images/categories/phones/phone_6.png"; // TODO remove
+import phone2 from "../../assets/images/categories/phones/phone_4.png"; // TODO remove
 
 /**
  * Reset and set active link in li element
@@ -16,34 +22,124 @@ const resetAndSetActiveLink = (name) => {
 
 function ReturnsPage() {
 	const { name } = useParams();
+	const [returns, setReturns] = useState([]);
 
 	useEffect(() => {
 		resetAndSetActiveLink(name);
 	}, [name]);
 
+	/**
+	 * Initialize returns data
+	 * TODO : fetch data from API
+	 * TODO : toast error
+	 */
+	const initReturns = () => {
+		setReturns([
+			{
+				id: "0f789703-c647-4e9f-8507-e5d23df473ce",
+				date: "01-07-2021",
+				products: [
+					{
+						name: "Apple iPhone 12 Pro Max",
+						quantity: 1,
+						price: 1259.99,
+						image: phone1,
+					},
+					{
+						name: "Casque audio Sony WH-1000XM4",
+						quantity: 1,
+						price: 379.99,
+						image: headphone1,
+					},
+				],
+				reason: "Article endommagé",
+			},
+			{
+				id: "0f789703-c646-4e9f-8507-e5d23df473ce",
+				date: "01-07-2021",
+				products: [
+					{
+						name: "Apple iPhone 12 Pro Max",
+						quantity: 1,
+						price: 1259.99,
+						image: phone2,
+					},
+					{
+						name: "Casque audio Sony WH-1000XM4",
+						quantity: 1,
+						price: 379.99,
+						image: headphone2,
+					},
+				],
+				reason: "Taille non conforme",
+			},
+		]);
+	};
+
+	useEffect(() => {
+		initReturns();
+	}, []);
+
 	return (
-		<div id="content" className="p-4 p-md-5">
-			<SidebarPage />
-			<h2 className="mb-4">Sidebar #04</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-				mollit anim id est laborum.
-			</p>
-		</div>
+		<>
+			<ToastContainer />
+			<div id="content" className="p-4 p-md-5">
+				<HeaderPage />
+				<div className="container p-0">
+					{returns.length === 0 ? (
+						<div className="text-center mt-5">
+							<h3>Aucun retour de commande</h3>
+							<p>
+								Effectuez un retour de commande depuis la page{" "}
+								<Link style={{ fontStyle: "italic" }} to="/account/orders">
+									Mes commandes
+								</Link>
+							</p>
+						</div>
+					) : (
+						returns.map((ret) => (
+							<div key={ret.id} className="card mb-3">
+								<div className="card-header">
+									<h4>Numéro de commande : {ret.id}</h4>
+									<p>Date : {ret.date}</p>
+								</div>
+								<div className="card-body">
+									<h5>Produits :</h5>
+									<table className="table">
+										<thead>
+											<tr>
+												<th>Image</th>
+												<th>Nom du produit</th>
+												<th>Quantité</th>
+												<th>Prix</th>
+											</tr>
+										</thead>
+										<tbody>
+											{ret.products.map((product) => (
+												<tr key={product.name}>
+													<td>
+														<img
+															src={product.image}
+															alt={product.name}
+															style={{ width: "50px", height: "50px" }}
+														/>
+													</td>
+													<td>{product.name}</td>
+													<td>{product.quantity}</td>
+													<td>{product.price}€</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+									<h5>Raison du retour :</h5>
+									<p>{ret.reason}</p>
+								</div>
+							</div>
+						))
+					)}
+				</div>
+			</div>
+		</>
 	);
 }
 
