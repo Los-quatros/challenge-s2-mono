34ed1cc9a874af8ed7ed995482b1043dc8544a07
moncef-24s <m.saiki@24s.com>
2023-07-04 00:44:46 +0200

    s

--- challenge-payment/src/products/products.service.ts
+++ challenge-payment/src/products/products.service.ts
@@ -4,6 +4,10 @@ import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService {
-    constructor(@Inject('PRODUCTS_SERVICE') private readonly productsProxy: ClientProxy) { }
+    constructor(@Inject('PRODUCTS_SERVICE') private readonly productsProxy: ClientProxy) {}
+    
+    async getProductById(id: string) {
+        return await lastValueFrom(this.productsProxy.send('getProduct', id));
+    }
    
 }
