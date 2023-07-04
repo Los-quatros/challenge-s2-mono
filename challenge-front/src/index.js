74962a823d42aec165abf43c490a9900fa080305
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-04 17:28:44 +0200

    Fix backend

--- challenge-front/src/index.js
+++ challenge-front/src/index.js
@@ -2,27 +2,28 @@ import "font-awesome/css/font-awesome.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider
-      client={
-        new QueryClient({
-          defaultOptions: {
-            queries: {
-              refetchOnWindowFocus: false,
-            },
-          },
-        })
-      }
-    >
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider
+			client={
+				new QueryClient({
+					defaultOptions: {
+						queries: {
+							refetchOnWindowFocus: false,
+						},
+					},
+				})
+			}
+		>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
 );
