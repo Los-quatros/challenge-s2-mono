39acdfec4b6d9b6a08a63570c3e43b6897b24ca2
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-05 09:39:16 +0200

    Fix scripts

--- stop_all.sh
+++ stop_all.sh
@@ -7,9 +7,8 @@ docker-compose -f challenge-front/docker-compose.yml stop
 docker-compose -f challenge-gateway/docker-compose.yml stop
 docker-compose -f challenge-mailer/docker-compose.yml stop
 docker-compose -f challenge-orders/docker-compose.yml stop
-docker-compose -f challenge-payments/docker-compose.yml stop
+docker-compose -f challenge-payment/docker-compose.yml stop
 docker-compose -f challenge-products/docker-compose.yml stop
-docker-compose -f challenge-publication/docker-compose.yml stop
 docker-compose -f challenge-returns/docker-compose.yml stop
 docker-compose -f challenge-sellers/docker-compose.yml stop
 docker-compose -f challenge-users/docker-compose.yml stop
