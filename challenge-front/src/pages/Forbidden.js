6d57e1b71e04e5334d38f610ad207a1601c0204f
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-06 16:31:05 +0200

    Spelling

--- challenge-front/src/pages/Forbidden.js
+++ challenge-front/src/pages/Forbidden.js
@@ -1,15 +1,16 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import forbidden from "../assets/styles/forbidden.module.css";
+
 function Forbidden() {
-  return (
-    <div className={forbidden["body-forbidden"]}>
-      <div className={forbidden.container}>
-        <h1>Sorry folks, page is forbidden.</h1>
-        <p>The moose out front shoulda told ya.</p>
-        <a href="/">Go back to home</a>
-      </div>
-    </div>
-  );
+	return (
+		<div className={forbidden["body-forbidden"]}>
+			<div className={forbidden.container}>
+				<h1>Désolé, vous n'avez pas accès à cette page.</h1>
+				<Link to="/">Retour à l'accueil</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Forbidden;
