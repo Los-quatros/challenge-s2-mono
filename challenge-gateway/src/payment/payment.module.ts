7c86812948b57bcf509c17c13d64409e10222bad
YacineSoussi <Yacine1.soussi@gmail.com>
2023-07-06 20:37:18 +0200

    Update payment.module.ts

--- challenge-gateway/src/payment/payment.module.ts
+++ challenge-gateway/src/payment/payment.module.ts
@@ -2,23 +2,27 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-    imports: [ClientsModule.register([
-        {
-            name: 'PAYMENTS_SERVICE',
-            transport: Transport.RMQ,
-            options: {
-                urls: [`amqp://rmq-service:5672`],
-                queue: 'payments_queue',
-                queueOptions: {
-                    durable: false
-                }
-            }
-        }
-    ])],
-    controllers: [PaymentController],
-    providers: [PaymentService],
-    exports: [PaymentService]
+  imports: [
+    UsersModule,
+    ClientsModule.register([
+      {
+        name: 'PAYMENTS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [`amqp://rmq-service:5672`],
+          queue: 'payments_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [PaymentController],
+  providers: [PaymentService],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
