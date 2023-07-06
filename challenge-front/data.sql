1fe949e052f546c21da21e8f990f2956de0ffe5d
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-06 03:03:43 +0200

    Add sql file for seeding

---
+++ challenge-front/data.sql
@@ -0,0 +1,239 @@
+-- HEADPHONES PRODUCTS --
+
+INSERT INTO "product" ("id", "label", "price", "quantity", "sellerId", "isActivated", "image", "categoryId", "description")
+VALUES
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120002',
+    'Casque sans fil Sony WH-1000XM4',
+    349.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '72753dd4-956d-42c8-9d51-06a6c89f532b',
+    'Le casque sans fil Sony WH-1000XM4 offre une expérience audio immersive avec une qualité sonore exceptionnelle. Doté de la technologie de réduction de bruit active, il vous permet de vous plonger dans votre musique préférée sans être dérangé par les bruits extérieurs. Sa batterie longue durée vous offre une écoute sans interruption pendant des heures. De plus, il est doté d"un design confortable et élégant, avec des commandes intuitives pour une utilisation facile.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120003',
+    'Casque audio Bose QuietComfort 35 II',
+    299.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '72753dd4-956d-42c8-9d51-06a6c89f532b',
+    'Le casque audio Bose QuietComfort 35 II offre un son exceptionnel et une réduction de bruit de premier ordre. Profitez de votre musique avec une clarté et une précision incroyables, tout en bloquant les bruits indésirables. Avec sa connectivité sans fil Bluetooth, vous pouvez écouter vos morceaux préférés en toute liberté. De plus, il est doté d"un design léger et confortable, idéal pour une utilisation prolongée.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120004',
+    'Casque sans fil Apple AirPods Pro',
+    249.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '72753dd4-956d-42c8-9d51-06a6c89f532b',
+    'Les casques sans fil Apple AirPods Pro offrent une expérience audio exceptionnelle avec un son immersif et une réduction de bruit active. Profitez de vos chansons préférées avec une qualité sonore exceptionnelle et une clarté incroyable. Les AirPods Pro sont également résistants à l"eau et à la transpiration, ce qui les rend parfaits pour une utilisation pendant les séances d"entraînement ou les activités sportives. De plus, leur boîtier de recharge offre une autonomie prolongée pour une écoute sans interruption.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120005',
+    'Casque gaming SteelSeries Arctis 7',
+    149.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '72753dd4-956d-42c8-9d51-06a6c89f532b',
+    'Le casque gaming SteelSeries Arctis 7 est spécialement conçu pour offrir une expérience de jeu immersive. Avec sa technologie audio de qualité supérieure, il vous plonge au cœur de l"action avec un son clair et détaillé. Son microphone rétractable vous permet de communiquer avec vos coéquipiers sans avoir à retirer le casque. De plus, il est doté d"un design léger et confortable, idéal pour une utilisation prolongée.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120006',
+    'Casque audio Sennheiser HD 660 S',
+    499.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '72753dd4-956d-42c8-9d51-06a6c89f532b',
+    'Le casque audio Sennheiser HD 660 S offre une qualité sonore exceptionnelle et un confort optimal. Grâce à ses transducteurs haute performance et à sa conception ouverte, il vous permet de profiter de votre musique préférée avec une clarté et une précision incroyables. De plus, il est doté d"un design léger et confortable, idéal pour une utilisation prolongée'
+  );
+
+-- PHONES PRODUCTS --
+
+INSERT INTO "product" ("id", "label", "price", "quantity", "sellerId", "isActivated", "image", "categoryId", "description")
+VALUES
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120014',
+    'Téléphone intelligent Samsung Galaxy S20',
+    999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '81313d61-33ba-459c-b4c9-6cd1ad4f1adb',
+    'Le téléphone intelligent Samsung Galaxy S20 vous offre une expérience mobile exceptionnelle avec un écran Infinity-O de 6,2 po et un appareil photo à triple objectif de 64 Mpx. Profitez d"une connectivité 5G ultra-rapide et d"une batterie longue durée pour une utilisation quotidienne sans souci. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120015',
+    'Téléphone intelligent Apple iPhone 11 Pro',
+    1379.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '81313d61-33ba-459c-b4c9-6cd1ad4f1adb',
+    'Le téléphone intelligent Apple iPhone 11 Pro offre une expérience mobile exceptionnelle avec un écran Super Retina XDR de 5,8 po et un appareil photo à triple objectif de 12 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce A13 Bionic et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120016',
+    'Téléphone intelligent Google Pixel 4',
+    999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '81313d61-33ba-459c-b4c9-6cd1ad4f1adb',
+    'Le téléphone intelligent Google Pixel 4 offre une expérience mobile exceptionnelle avec un écran OLED de 5, 7 po et un appareil photo à double objectif de 16 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce Snapdragon 855 et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120017',
+    'Téléphone intelligent Samsung Galaxy Note10+',
+    1319.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '81313d61-33ba-459c-b4c9-6cd1ad4f1adb',
+    'Le téléphone intelligent Samsung Galaxy Note10+ vous offre une expérience mobile exceptionnelle avec un écran Infinity-O de 6,8 po et un appareil photo à quadruple objectif de 16 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce Snapdragon 855 et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120018',
+    'Téléphone intelligent Apple iPhone 11',
+    979.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '81313d61-33ba-459c-b4c9-6cd1ad4f1adb',
+    'Le téléphone intelligent Apple iPhone 11 offre une expérience mobile exceptionnelle avec un écran Liquid Retina HD de 6,1 po et un appareil photo à double objectif de 12 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce A13 Bionic et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  );
+
+-- TABLETS PRODUCTS --
+
+INSERT INTO "product" ("id", "label", "price", "quantity", "sellerId", "isActivated", "image", "categoryId", "description")
+VALUES
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120024',
+    'Tablette Apple iPad Pro 11 po 64 Go',
+    999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '548bcd71-6109-40dc-b98b-9670c2e9c1e8',
+    'La tablette Apple iPad Pro 11 po 64 Go offre une expérience mobile exceptionnelle avec un écran Liquid Retina de 11 po et un appareil photo de 12 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce A12X Bionic et à la batterie longue durée. De plus, elle est dotée d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120025',
+    'Tablette Samsung Galaxy Tab S6 10,5 po 128 Go',
+    999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '548bcd71-6109-40dc-b98b-9670c2e9c1e8',
+    'La tablette Samsung Galaxy Tab S6 10,5 po 128 Go offre une expérience mobile exceptionnelle avec un écran Super AMOLED de 10,5 po et un appareil photo à double objectif de 13 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce Snapdragon 855 et à la batterie longue durée. De plus, elle est dotée d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120026',
+    'Tablette Apple iPad 10,2 po 32 Go',
+    429.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '548bcd71-6109-40dc-b98b-9670c2e9c1e8',
+    'La tablette Apple iPad 10,2 po 32 Go offre une expérience mobile exceptionnelle avec un écran Retina de 10,2 po et un appareil photo de 8 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce A10 Fusion et à la batterie longue durée. De plus, elle est dotée d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120027',
+    'Tablette Samsung Galaxy Tab A 10,1 po 32 Go',
+    299.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '548bcd71-6109-40dc-b98b-9670c2e9c1e8',
+    'La tablette Samsung Galaxy Tab A 10,1 po 32 Go offre une expérience mobile exceptionnelle avec un écran WUXGA de 10,1 po et un appareil photo de 8 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce Exynos 7904 et à la batterie longue durée. De plus, elle est dotée d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120028',
+    'Tablette Samsung Galaxy Tab A 8 po 32 Go',
+    199.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '548bcd71-6109-40dc-b98b-9670c2e9c1e8',
+    'La tablette Samsung Galaxy Tab A 8 po 32 Go offre une expérience mobile exceptionnelle avec un écran WXGA de 8 po et un appareil photo de 8 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce Snapdragon 429 et à la batterie longue durée. De plus, elle est dotée d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  );
+
+-- CAMERAS PRODUCTS --
+
+INSERT INTO "product" ("id", "label", "price", "quantity", "sellerId", "isActivated", "image", "categoryId", "description")
+VALUES
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120034',
+    'Appareil photo sans miroir Sony Alpha a7 III avec objectif 28-70 mm',
+    2999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '90543b10-d3f6-4801-afd6-ea9a49b58e64',
+    'L"appareil photo sans miroir Sony Alpha a7 III avec objectif 28-70 mm offre une expérience mobile exceptionnelle avec un écran LCD de 3 po et un appareil photo de 24,2 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce BIONZ X et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120035',
+    'Appareil photo sans miroir Sony Alpha a7R IV avec objectif 24-105 mm',
+    5999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '90543b10-d3f6-4801-afd6-ea9a49b58e64',
+    'L"appareil photo sans miroir Sony Alpha a7R IV avec objectif 24-105 mm offre une expérience mobile exceptionnelle avec un écran LCD de 3 po et un appareil photo de 61 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce BIONZ X et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120036',
+    'Appareil photo sans miroir Sony Alpha a7R III avec objectif 24-70 mm',
+    4499.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '90543b10-d3f6-4801-afd6-ea9a49b58e64',
+    'L"appareil photo sans miroir Sony Alpha a7R III avec objectif 24-70 mm offre une expérience mobile exceptionnelle avec un écran LCD de 3 po et un appareil photo de 42,4 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce BIONZ X et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120037',
+    'Appareil photo sans miroir Sony Alpha a7 II avec objectif 28-70 mm',
+    1999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '90543b10-d3f6-4801-afd6-ea9a49b58e64',
+    'L"appareil photo sans miroir Sony Alpha a7 II avec objectif 28-70 mm offre une expérience mobile exceptionnelle avec un écran LCD de 3 po et un appareil photo de 24,3 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce BIONZ X et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  ),
+  (
+    '51b3e54e-1b93-11ee-be56-0242ac120038',
+    'Appareil photo sans miroir Sony Alpha a7S II avec objectif 28-70 mm',
+    2999.99,
+    5,
+    NULL,
+    TRUE,
+    NULL,
+    '90543b10-d3f6-4801-afd6-ea9a49b58e64',
+    'L"appareil photo sans miroir Sony Alpha a7S II avec objectif 28-70 mm offre une expérience mobile exceptionnelle avec un écran LCD de 3 po et un appareil photo de 12,2 Mpx. Profitez d"une performance fluide et d"une autonomie prolongée grâce à la puce BIONZ X et à la batterie longue durée. De plus, il est doté d"un design élégant et durable qui résiste à l"eau et à la poussière.'
+  );
