8cb5203c3ebaa373b71b97fd6c552d5b516ef0f3
Pereira Júlio <julioocesarr@outlook.fr>
2023-07-29 15:43:22 +0200

    Update README file

--- start_all.sh
+++ start_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #if permission denied : chmod +x start_all.sh
 
-docker-compose -f challenge-addresses/docker-compose.yml up -d 
-docker-compose -f challenge-carriers/docker-compose.yml up -d 
-docker-compose -f challenge-front/docker-compose.yml up -d 
-docker-compose -f challenge-gateway/docker-compose.yml up -d 
-docker-compose -f challenge-mailer/docker-compose.yml up -d 
-docker-compose -f challenge-images/docker-compose.yml up -d 
-docker-compose -f challenge-orders/docker-compose.yml up -d 
-docker-compose -f challenge-payment/docker-compose.yml up -d 
-docker-compose -f challenge-products/docker-compose.yml up -d 
-docker-compose -f challenge-returns/docker-compose.yml up -d 
-docker-compose -f challenge-sellers/docker-compose.yml up -d 
-docker-compose -f challenge-users/docker-compose.yml up -d 
+docker-compose -f challenge-addresses/docker-compose.yml up -d
+docker-compose -f challenge-carriers/docker-compose.yml up -d
+docker-compose -f challenge-front/docker-compose.yml up -d
+docker-compose -f challenge-gateway/docker-compose.yml up -d
+docker-compose -f challenge-mailer/docker-compose.yml up -d
+docker-compose -f challenge-images/docker-compose.yml up -d
+docker-compose -f challenge-orders/docker-compose.yml up -d
+docker-compose -f challenge-payment/docker-compose.yml up -d
+docker-compose -f challenge-products/docker-compose.yml up -d
+docker-compose -f challenge-returns/docker-compose.yml up -d
+docker-compose -f challenge-sellers/docker-compose.yml up -d
+docker-compose -f challenge-users/docker-compose.yml up -d
