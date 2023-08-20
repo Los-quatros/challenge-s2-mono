47cc4c6eb7b7919099bc171f1212b2be2cb62d79
YacineSoussi <Yacine1.soussi@gmail.com>
2023-08-20 13:08:30 +0200

    test mailer change

--- challenge-mailer/src/main.ts
+++ challenge-mailer/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from "@nestjs/microservices";
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: [`amqp://rmq-service:5672`],
+      urls: [`amqp://34.76.248.98:5672`],
       queue: 'mailer_queue',
       queueOptions: {
-        durable: false
-      }
-    }
+        durable: false,
+      },
+    },
   });
-  
+
   await app.listen();
 }
 bootstrap();
