0e6ac2bbeedb111a761824e69a90fec33d7ecb13
YacineSoussi <Yacine1.soussi@gmail.com>
2023-08-29 22:35:35 +0200

    Update addresses.controller.ts

--- challenge-addresses/src/addresses/addresses.controller.ts
+++ challenge-addresses/src/addresses/addresses.controller.ts
@@ -7,30 +7,36 @@ import { CreateAddressDto } from './models/CreateAddressDto';
 
 @Controller('addresses')
 export class AddressesController {
-    constructor(private addressesService: AddressesService) {}
+  constructor(private addressesService: AddressesService) {}
 
-    @EventPattern("CreateAddress")
-    async CreateAddress(@Payload() createAddressDto : CreateAddressDto) {
-        return this.addressesService.CreateAddress(createAddressDto['data']);
-    }
+  @EventPattern('CreateAddress')
+  async CreateAddress(@Payload() createAddressDto: CreateAddressDto) {
+    return this.addressesService.CreateAddress(createAddressDto['data']);
+  }
 
-    @EventPattern("GetAddressById")
-    async GetAddressById(@Payload() addressId : string) : Promise<Address>{
-        return this.addressesService.GetAddressById(addressId['idAddress']);
-    }
+  @EventPattern('GetAddressById')
+  async GetAddressById(@Payload() addressId: string): Promise<Address> {
+    return this.addressesService.GetAddressById(addressId['idAddress']);
+  }
 
-    @EventPattern("GetUserAddresses")
-    async GetUserAddresses(@Payload() idUser : string) : Promise<Array<Address>> {
-        return this.addressesService.GetUserAddresses(idUser['id']);
-    }
-    @EventPattern("DeleteAddress")
-    async DeleteAddressById(@Payload() idAddress : string) {
-        return this.addressesService.DeleteAddressById(idAddress['id']);
-    }
+  @EventPattern('GetUserAddresses')
+  async GetUserAddresses(@Payload() idUser: string): Promise<Array<Address>> {
+    console.log('ControllerAdresse');
+    return this.addressesService.GetUserAddresses(idUser['id']);
+  }
+  @EventPattern('DeleteAddress')
+  async DeleteAddressById(@Payload() idAddress: string) {
+    return this.addressesService.DeleteAddressById(idAddress['id']);
+  }
 
-    @EventPattern("ChangeAddress")
-    async ChangeAddress(@Payload() data: ChangeAddressDto, @Payload() idAddress : string) {
-        return this.addressesService.ChangeAddress(data['data'], idAddress['idAddress']);
-    }
-
+  @EventPattern('ChangeAddress')
+  async ChangeAddress(
+    @Payload() data: ChangeAddressDto,
+    @Payload() idAddress: string,
+  ) {
+    return this.addressesService.ChangeAddress(
+      data['data'],
+      idAddress['idAddress'],
+    );
+  }
 }
