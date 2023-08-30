df74d70e4ba00b20f5d94fe0d16b61cb19235b02
YacineSoussi <Yacine1.soussi@gmail.com>
2023-08-30 21:24:42 +0200

    Update App.js

--- challenge-front/src/App.js
+++ challenge-front/src/App.js
@@ -82,6 +82,7 @@ const clearLinks = () => {
 };
 
 const AppContent = () => {
+  console.log(process.env.REACT_APP_BASE_API_URL, 'API URL');
   const hasToken = localStorage.getItem('token') ? true : false;
   const [isAdmin, setIsAdmin] = useState(false);
   const location = useLocation();
